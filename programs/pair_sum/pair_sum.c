81229c5e4ad12b33daecc4820e8a32376fdee7e5
ANTWNIOS DIMAKIS <sdi1900047@linux11.di.uoa.gr>
2020-03-10 10:58:59 +0200

    commit

--- programs/pair_sum/pair_sum.c
+++ programs/pair_sum/pair_sum.c
@@ -6,7 +6,7 @@
 Pair pair_sum(int target, Vector numbers) {
 	VectorNode node1, node2;
 	Pair sol;
-	sol = malloc(sizeof());
+	sol = malloc(sizeof(struct pair));
 	for (node1 = vector_first(numbers) ; node1 != vector_last(numbers) ; node1 = vector_next(numbers, node1)) {
 		for (node2 = vector_next(numbers, node1) ; node2 != VECTOR_EOF ; node2 = vector_next(numbers, node2)) {
 			if (target == (*(int*)vector_node_value(numbers, node1) + *(int*)vector_node_value(numbers, node2))) {
