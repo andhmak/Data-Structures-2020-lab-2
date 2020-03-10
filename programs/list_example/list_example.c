3365de55386d4379af9c7b7c2b36fd46c8c9cf95
ANTWNIOS DIMAKIS <sdi1900047@linux11.di.uoa.gr>
2020-03-10 10:21:26 +0200

    commit

--- programs/list_example/list_example.c
+++ programs/list_example/list_example.c
@@ -3,18 +3,23 @@
 
 #include "ADTList.h"
 
-
+int* create_int(int i) {
+	int* p = malloc(sizeof(int));
+	*p = i;
+	return p;
+}
 
 int main() {
-	List list = list_create(NULL);
+	List list = list_create(free);
 
 	// προσθήκη των αριθμών από το 0 έως 9 στη λίστα
 	for (int i = 0; i < 10; i++)
-		list_insert_next(list, list_last(list), &i);
+		list_insert_next(list, list_last(list), create_int(i));
 
 	// εκτύπωση των περιεχομένων της λίστας
-	//
-	// for (...) {
-	// 	...
-	// }
+	
+	for (ListNode node = list_first(list) ; node != LIST_EOF ; node = list_next(list, node)) {
+		printf("%d\n", *(int*)list_node_value(list, node));
+	}
+	list_destroy(list);
 } 
